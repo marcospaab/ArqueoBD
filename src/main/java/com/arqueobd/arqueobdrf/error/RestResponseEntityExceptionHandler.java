a722c2ab0b7b43d0798fffbde285643338b53c4e
marcospaab <marcos.padin@outlook.es>
2025-03-10 12:48:35 +0100

    Creación de validaciones para name y code de yacimiento.
    Se le crea también una excepción personalizada para estas validaciones.

--- src/main/java/com/arqueobd/arqueobdrf/error/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/arqueobd/arqueobdrf/error/RestResponseEntityExceptionHandler.java
@@ -1,12 +1,19 @@
 package com.arqueobd.arqueobdrf.error;
 
 import com.arqueobd.arqueobdrf.error.dto.ErrorMessage;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
@@ -16,5 +23,16 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
     public ResponseEntity<ErrorMessage> siteNotFoundException(SiteNotFoundException exception){
         ErrorMessage message = new ErrorMessage(HttpStatus.NOT_FOUND, exception.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(message);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        Map<String,Object> errors = new HashMap<>();
+        //de la excepcion se obtiene la lista de excepciones y errores y se está recorriendo, por cada error se añade en el mapa
+        //el campo de ese errory entregando el mensaje personalizado que configuramos
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
 }
